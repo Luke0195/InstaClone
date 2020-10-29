b6b4d03c7bcac1bd7355c5e1e36f04919a2573f3
Luke0195 <lucas201273@gmail.com>
2020-10-29 08:59:17 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text, StyleSheet, StatusBar, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return <></>;
+}
+
+const styles = StyleSheet.create({});
